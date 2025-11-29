6617421d641276a9c8a70a2edf1124a0922055ac
lok <lok_ziming@outlook.com>
2025-11-29 16:15:16 +0800

    调整跨域规则

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ app.add_middleware(RequestIDMiddleware)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=(["*"] if settings.debug else ["http://localhost:3000"]),
+    allow_origins=(["*"]),
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
