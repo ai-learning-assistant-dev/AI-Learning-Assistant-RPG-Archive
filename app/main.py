dc9604509d4167f5fc43b5d90058ac525a5b3020
lok <lok_ziming@outlook.com>
2025-11-29 15:49:42 +0800

    只监听localhost的来源ip

--- app/main.py
+++ app/main.py
@@ -58,9 +58,7 @@ app.add_middleware(RequestIDMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=(
-        ["*"]
-        if settings.debug
-        else ["http://localhost:3000", "http://127.0.0.1:3000", "http://0.0.0.0:3000"]
+        ["*"] if settings.debug else ["http://localhost:3000", "http://127.0.0.1:3000"]
     ),
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
